30c8c3da0721628443b7addb34ff7c2198afc23e
DeadlyDiva58 <deadlydiva58@gmail.com>
2025-11-25 23:32:10 -0500

    Updated all the stuff part 1

--- Assets/NewController.cs
+++ Assets/NewController.cs
@@ -50,7 +50,7 @@ public class Controller : MonoBehaviour
         //Quaternion deltaRotation = Quaternion.Euler(0f, horizontalInput * rotationSpeed * Time.deltaTime, 0f);
         Vector3 forwardInput = new Vector3(0, 0, Input.GetAxis("Vertical"));
         //rb.MoveRotation(rb.rotation*deltaRotation);
-        rb.MovePosition(transform.position + forwardInput * Time.fixedDeltaTime * speed);*/
+        rb.MovePosition(transform.position + forwardInput * Time.fixedDeltaTime * speed);
         float moveInput = Input.GetAxis("Vertical");   // W/S or Up/Down arrows
         float rotateInput = Input.GetAxis("Horizontal"); // A/D or Left/Right arrows
 
@@ -61,7 +61,21 @@ public class Controller : MonoBehaviour
 
         // Calculate movement in the forward direction
         Vector3 movement = transform.forward * moveInput * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + movement);
+        rb.MovePosition(rb.position + movement);*/
+        
+        
+            float moveInput = Input.GetAxis("Vertical");
+            float rotateInput = Input.GetAxis("Horizontal");
+
+            // Rotation
+            float rotation = rotateInput * rotationSpeed * Time.fixedDeltaTime;
+            Quaternion deltaRotation = Quaternion.Euler(0f, rotation, 0f);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+
+            // Movement using velocity (respects collisions)
+            Vector3 movement = transform.forward * moveInput * speed;
+            rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+        
     }
 
 }
